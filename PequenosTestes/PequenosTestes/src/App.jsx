d20fd9c1b8accf48f60263c7c9f3ffc4e2d1a23e
clerio <cleririo@gmail.com>
2023-06-04 14:46:29 -0300

    Testes em casa

---
+++ PequenosTestes/PequenosTestes/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  //Filter
+  /*const livros = [
+    {titulo: "Construindo Sites com HTML", autor: "Mauricio Samy Silva"},
+    {titulo: "Web Scraping com Python", autor: "Ryan Mitchell"},
+    {titulo: "CSS3", autor: "Mauricio Samy Silva"} 
+  ]; 
+
+  let livros1 = livros.filter((livro) => livro.titulo === "CSS3");
+  let livros2 = livros.filter((livro) => livro.autor === "Ryan Mitchell");
+  let livros3 = livros.filter((livro) => livro.titulo.includes("com"));
+
+  console.log(livros1);
+  console.log(livros2);
+  console.log(livros3);*/
+
+  //Find
+  /*const livros = [
+    {id: 1, titulo: "Construindo Sites com HTML"},
+    {id: 2, titulo: "Web Scraping com Python"},
+    {id: 3, titulo: "CSS3"}
+  ];
+
+  let livros1 = livros.find((livro) => livro.id ===3);
+
+  console.log(livros1);
+  console.log(livros1.titulo);
+  */
+}
+
+export default App
